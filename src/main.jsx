6d95bc5b798f95fb350315f9dbe776cdb4a17dae
Shrey Kumar Gupta <shreykr.gupta1260@gmail.com>
2026-06-10 23:49:32 +0530

    fix github pages deployment configuration

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import "./styles/layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/PROJECT_HARMONICS">
             <App />
         </BrowserRouter>
     </React.StrictMode>
